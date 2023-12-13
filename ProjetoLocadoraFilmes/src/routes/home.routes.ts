b4a2ba4110834207093bf0e1b12fd33e0afc8726
thiagojomar <143623085+thiagojomar@users.noreply.github.com>
2023-12-13 09:22:20 -0300

    att projeto locadora
    configuração e criação de rotas

---
+++ ProjetoLocadoraFilmes/src/routes/home.routes.ts
@@ -0,0 +1,16 @@
+import { Router, Request, Response } from "express";
+//import { welcome } from "../controllers/home.controller";
+
+class HomeRoutes {
+  router = Router();
+
+  constructor() {
+    this.intializeRoutes();
+  }
+
+  intializeRoutes() {
+    this.router.get("/", (req: Request, res: Response) => { return res.status(200).json("Olá mundo!") });
+  }
+}
+
+export default new HomeRoutes().router;
